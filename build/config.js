3ef70b2f695aa240ad1f7e90f3947cf180b0ab15
Gijs Nijholt <gijs.nijholt@gmail.com>
2015-04-10 10:47:11 +0200

    Adding config and weightsettings

---
+++ build/config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.dashboardName = 'Almere';
+config.cityName = 'Almere';
+config.cityGeojsonFile = 'stadsdelen_almere.geojson';
+config.neighborhoodsGeojsonFile = 'almere_4326_simple.geojson';
+config.lat = 5.3608;
+config.lon = 52.3641;
+
+config.csvFileKPI = 'KPI.csv';
+config.csvFileGebruik = 'data_gebruik.csv';
+config.csvFileTevredenheid = 'data_tevredenheid.csv';
+config.csvFileToestand = 'data_toestand.csv';
+config.csvFilePlanning = 'data_planrealisatie.csv';
+config.csvFileOmgeving = 'data_omgevingseffect.csv';
+
+
+module.exports = config;
